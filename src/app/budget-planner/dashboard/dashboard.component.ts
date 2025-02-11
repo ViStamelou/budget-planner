679d34e03a9457be7dc637e33c05027a7e79b30f
ViStamelou <vasilikist.97@gmail.com>
2025-02-11 20:29:22 +0200

    add files

---
+++ src/app/budget-planner/dashboard/dashboard.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { SideNavComponent } from '../side-nav/side-nav.component';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [MatIconModule, SideNavComponent,CommonModule],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss'
+})
+export class DashboardComponent {
+  //Income
+  lastMonthsIncome = ['September:1200€','October:1240€', 'November:1370€'];
+  currentMonthIncome = '1600€';
+
+  //Expense
+  lastMonthsExpense = ['September:750€','October:750€', 'November:890€'];
+  currentMonthExpense = '1000€';
+
+  //To Do
+  todoTransactions = [
+    {description:'Pay phone & cellphone'},
+    {description:'Pay maintenance fees'},
+    {description:'Buy groceries'},
+    {description:'Pay credit card'}
+  ];
+
+  totalCurrentMonthIncome = 1600;
+  totalCurrentMonthExpense = 1000;
+
+  constructor(public router:Router){}
+  onIncome() {
+    this.router.navigate(['/budget-planner/income']);
+  }
+
+  onExpense() {
+    this.router.navigate(['/budget-planner/expense']);
+  }
+
+  onTodo() {
+    this.router.navigate(['/budget-planner/todo']);
+  }
+
+  get currentMonthSavings(): number{
+    return this.totalCurrentMonthIncome - this.totalCurrentMonthExpense;
+  }
+}
---
+++ src/app/budget-planner/income/income.component.ts
@@ -0,0 +1,127 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-income',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './income.component.html',
+  styleUrl: './income.component.scss'
+})
+export class IncomeComponent {
+  incomeForm: any;
+  selectedMonth: any;
+  decemberIncomes: any[] = [
+    { source: 'Salary', amount: 800},
+    { source: 'Freelancing', amount: 1000}
+  ];  
+  januaryIncomes: any[] = [
+    { source: 'Tutoring', amount: 400},
+    { source: 'Freelancing', amount: 1300}
+  ];
+  februaryIncomes: any[] = [
+    { source: 'Salary', amount: 800},
+    { source: 'Tutoring', amount: 450}
+  ];
+  marchIncomes: any[] = [
+    { source: 'Salary', amount: 800},
+    { source: 'Freelancing', amount: 1000},
+    { source: 'Tutoring', amount: 450}
+  ];
+  monthSelected:boolean=false;
+  constructor(public fb: FormBuilder,public router:Router) { 
+    const currentDate = new Date();
+    this.selectedMonth = currentDate.toLocaleString('default', { month: 'long' });
+  }
+  ngOnInit(): void {
+    this.incomeForm = this.fb.group({
+      month: ['', Validators.required],
+      source: ['', Validators.required],
+      amount: ['', [Validators.required, Validators.min(1)]]
+    });
+  }
+
+  onChange(event: any) {
+    this.selectedMonth = event.target.value
+    this.monthSelected=true;
+    this.getFilteredIncomes();
+  }
+
+  calculateTotalIncome(month: string): number {
+    let totalIncome = 0;
+    for (const income of this.getIncomesForMonth(month)) {
+      totalIncome += income.amount;
+    }
+    return totalIncome;
+  }
+
+  getIncomesForMonth(month: string): any[] {
+    switch (month) {
+      case 'December':
+        return this.decemberIncomes;
+      case 'January':
+        return this.januaryIncomes;
+      case 'February':
+        return this.februaryIncomes;
+      case 'March':
+        return this.marchIncomes;
+      default:
+        return [];
+    }
+  }
+
+  getFilteredIncomes() {
+    let filteredIncomes: any[] = [];
+    switch (this.selectedMonth) {
+      case 'December':
+        filteredIncomes = [...this.decemberIncomes];
+        break;
+      case 'January':
+        filteredIncomes = [...this.januaryIncomes];
+        break;
+      case 'February':
+        filteredIncomes = [...this.februaryIncomes];
+        break;
+      case 'March':
+        filteredIncomes = [...this.marchIncomes];
+        break;
+      default:
+        break;
+    }
+    return filteredIncomes;
+  }
+  onSubmit() {
+    if (this.incomeForm.valid) {
+      const newIncome = this.incomeForm.value;
+      switch (this.selectedMonth) {
+        case 'December':
+          this.decemberIncomes.push(newIncome);
+          break;
+        case 'January':
+          this.januaryIncomes.push(newIncome);
+          break;
+        case 'February':
+          this.februaryIncomes.push(newIncome);
+          break;
+        case 'March':
+          this.marchIncomes.push(newIncome);
+          break;
+        default:
+          break;
+      }
+      this.incomeForm.reset();
+      this.incomeForm.patchValue({ month: '', source: '', amount: ''});
+    }
+  }
+
+  saveForm() {
+    console.log("Form saved!");
+  }
+
+  onBack() {
+    this.router.navigate(['/budget-planner/dashboard']);
+  }
+}
+
